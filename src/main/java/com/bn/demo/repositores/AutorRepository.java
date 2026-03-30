2acd0e52d0996a271852c66e65e96b307c2f0edc
gabrielgalhar <galhardonefelipegabriel@gmail.com>
2026-03-30 20:12:47 -0300

    ajustado

--- src/main/java/com/bn/demo/repositores/AutorRepository.java
+++ src/main/java/com/bn/demo/repositores/AutorRepository.java
@@ -2,6 +2,8 @@ package com.bn.demo.repositores;
 
 import com.bn.demo.entities.Autor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AutorRepository extends JpaRepository<Autor, Long> {
 }
