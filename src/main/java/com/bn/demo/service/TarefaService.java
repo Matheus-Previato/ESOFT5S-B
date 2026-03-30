575c29d0dd67ada2d379d98ae730509b29a39ecc
Filipe Boligon <filiperuizboligon9@gmail.com>
2026-03-30 20:03:24 -0300

    feat: service

---
+++ src/main/java/com/bn/demo/service/TarefaService.java
@@ -0,0 +1,39 @@
+package com.bn.demo.service;
+
+import com.bn.demo.model.Tarefa;
+import com.bn.demo.repository.TarefaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class TarefaService {
+
+    @Autowired
+    private TarefaRepository tarefaRepository;
+
+    public void salvar(Tarefa tarefa){
+        tarefaRepository.save(tarefa);
+    }
+
+    public void excluir(Long id){
+        tarefaRepository.deleteById(id);
+    }
+
+    public Optional<Tarefa> buscarPorId(Long id){
+        return tarefaRepository.findById(id);
+    }
+
+    public Iterable<Tarefa> buscarTodos(){
+        return tarefaRepository.findAll();
+    }
+
+    public void atualizar(Long id, Tarefa tarefa){
+        Tarefa entity = tarefaRepository.findById(id).get();
+        entity.setDescricao(tarefa.getDescricao());
+        entity.setDataVencimento(tarefa.getDataVencimento());
+        entity.setConcluida(tarefa.getConcluida());
+    }
+
+}
